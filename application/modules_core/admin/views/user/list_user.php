a8e32b2e612bae99bd5f75975349bca79721a0a5
nosnevetzy <yztevensonbier@rocketmail.com>
2017-08-12 16:38:59 -0700

    Fix page header space in content

--- application/modules_core/admin/views/user/list_user.php
+++ application/modules_core/admin/views/user/list_user.php
@@ -1,59 +1,66 @@
 <?php $this->load->view(admin_dir('template/header')); ?>
 
-<div class="page-header">
-    <div class="page-header-content">
-        <div class="page-title">
-            <h5>
-                <span class="text-semibold"><i class="icon-list-unordered mr-5"></i> User List</span>
-            </h5>
-        </div>
+<!-- Main content -->
+<div class="content-wrapper">
 
-        <div class="heading-elements">
-            <div class="btn-group heading-btn">
-                <button class="btn btn-icon btn-sm dropdown-toggle legitRipple" data-toggle="dropdown" aria-expanded="false">
-                    <i class="icon-wrench"></i>
-                    <span class="caret"></span>
-                </button>
+    <!-- Page header -->
+    <div class="page-header">
+        <div class="page-header-content">
+            <div class="page-title">
+                <h5>
+                    <span class="text-semibold"><i class="icon-list-unordered mr-5"></i> User List</span>
+                </h5>
+            </div>
 
-                <ul class="dropdown-menu dropdown-menu-right">
-                    <?php if ($this->Misc->accessible($this->access, 'user', 'method', 'add_user')) { ?>
-                        <li><a href="<?php echo admin_url("user/add_user"); ?>"><i class="icon-user-plus"></i> Add New User</a></li>
-                    <?php }
-                    ?>
-                    <?php if ($this->Misc->accessible($this->access, 'user', 'method', 'csv_export')) { ?>
-                        <li><a href="#" id="User List" class="csv_export"><i class="icon-file-excel"></i> Export Table</a></li>
-                    <?php } ?>
-                </ul>
+            <div class="heading-elements">
+                <div class="btn-group heading-btn">
+                    <button class="btn btn-icon btn-sm dropdown-toggle legitRipple" data-toggle="dropdown" aria-expanded="false">
+                        <i class="icon-wrench"></i>
+                        <span class="caret"></span>
+                    </button>
+
+                    <ul class="dropdown-menu dropdown-menu-right">
+                        <?php if ($this->Misc->accessible($this->access, 'user', 'method', 'add_user')) { ?>
+                            <li><a href="<?php echo admin_url("user/add_user"); ?>"><i class="icon-user-plus"></i> Add New User</a></li>
+                        <?php }
+                        ?>
+                        <?php if ($this->Misc->accessible($this->access, 'user', 'method', 'csv_export')) { ?>
+                            <li><a href="#" id="User List" class="csv_export"><i class="icon-file-excel"></i> Export Table</a></li>
+                        <?php } ?>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
-</div>
+    <!-- /page header -->
 
+    <!-- Content area -->
+    <div class="content">
 
-<!-- page content -->
-<div class="right_col" role="main" >
-    <div id="clearflash" class="">
-        <div class="col-md-12 col-sm-12 col-xs-12">
-            <div class="x_panel">
-                <div class="x_content">
-                    <div id='containerList'><center style="min-height: 100vh;"><img src='<?php echo assets_dir("images/animal.gif"); ?>' ></center></div>
-                </div><!-- end of .x_content -->
-            </div><!-- end of .x_panel -->
-        </div><!-- end of div -->
-    </div><!-- end of .clearflash -->
-</div><!-- end of .right_col -->
+    <!-- page content -->
+    <div class="right_col" role="main" >
+        <div id="clearflash" class="">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                    <div class="x_content">
+                        <div id='containerList'><center style="min-height: 100vh;"><img src='<?php echo assets_dir("images/animal.gif"); ?>' ></center></div>
+                    </div><!-- end of .x_content -->
+                </div><!-- end of .x_panel -->
+            </div><!-- end of div -->
+        </div><!-- end of .clearflash -->
+    </div><!-- end of .right_col -->
 
-<script type="text/javascript">
-    $(document).ready(function () {
-        load_datalist({action: "<?php echo admin_url('user/method/list_user'); ?>"});
+    <script type="text/javascript">
+        $(document).ready(function () {
+            load_datalist({action: "<?php echo admin_url('user/method/list_user'); ?>"});
 
-        $('#containerList').on('click', '.delete_user', {
-            'template': "<?php echo admin_url("template/confirmation"); ?>",
-            'action': "<?php echo admin_url("user/method/delete_user"); ?>",
-            'message': "You are about to delete this user.",
-            'redirect': "<?php echo current_url(); ?>"
-        }, load_dfltconfirmation);
-    });
-</script>
+            $('#containerList').on('click', '.delete_user', {
+                'template': "<?php echo admin_url("template/confirmation"); ?>",
+                'action': "<?php echo admin_url("user/method/delete_user"); ?>",
+                'message': "You are about to delete this user.",
+                'redirect': "<?php echo current_url(); ?>"
+            }, load_dfltconfirmation);
+        });
+    </script>
 
 <?php $this->load->view(admin_dir('template/footer')); ?>
