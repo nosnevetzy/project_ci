bb2e042667d815bdc75fe6079fd7d0d5188d494f
nosnevetzy <yztevensonbier@rocketmail.com>
2017-08-14 15:24:47 +0800

    Fix user type list page

--- application/modules_core/admin/views/user_type/list_user_type.php
+++ application/modules_core/admin/views/user_type/list_user_type.php
@@ -1,38 +1,51 @@
 <?php $this->load->view(admin_dir('template/header')); ?>
+<!-- Main content -->
+<div class="content-wrapper">
 
-<!-- page content -->
-<div class="right_col" role="main">
-    <div id="clearflash">
-        <div class="page-title">
-            <div class="clearfix"></div>
-        </div>
-        <div class="col-md-12 col-sm-12 col-xs-12">
-            <div class="x_panel">
-                <div class="x_title">
-                    <h2>User Type List</h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                        <li class="dropdown" style="float: right;">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench fa-2x"></i></a>
-                            <ul class="dropdown-menu" role="menu">
-                                <?php if ($this->Misc->accessible($this->access, 'user', 'method', 'add_user_type')) { ?>
-                                    <li style="padding: 5px 10px;"><a href="<?php echo admin_url("user_type/add_user_type"); ?>"><span class="icomoon-icon-plus"></span> Add New User Type</a></li>
-                                <?php }
-                                ?>
-                                <?php if ($this->Misc->accessible($this->access, 'user', 'method', 'csv_export')) { ?>
-                                    <li style="padding: 5px 10px;"><a href="#" id="User Type List <?php echo date("M-d-Y") ?>" class="csv_export"><span class="icomoon-icon-file-excel" style="pointer-events: cursor"></span> Export Table</a></li>
-                                <?php } ?>
-                            </ul>
-                        </li>
-                    </ul> 
-                    <div class="clearfix"></div>
+    <!-- Page header -->
+    <div class="page-header">
+        <div class="page-header-content">
+            <div class="page-title">
+                <h5>
+                    <span class="text-semibold"><i class="icon-list-unordered mr-5"></i> User Type List</span>
+                </h5>
+            </div>
+
+            <div class="heading-elements">
+                <div class="btn-group heading-btn">
+                    <button class="btn btn-icon btn-sm dropdown-toggle legitRipple" data-toggle="dropdown" aria-expanded="false">
+                        <i class="icon-wrench"></i>
+                        <span class="caret"></span>
+                    </button>
+
+                    <ul class="dropdown-menu dropdown-menu-right">
+                        <?php if ($this->Misc->accessible($this->access, 'user', 'method', 'add_user_type')) { ?>
+                            <li><a href="<?php echo admin_url("user_type/add_user_type"); ?>"><span class="icon-user-plus"></span> Add New User Type</a></li>
+                        <?php }
+                        ?>
+                        <?php if ($this->Misc->accessible($this->access, 'user', 'method', 'csv_export')) { ?>
+                            <li><a href="#" id="User Type List <?php echo date("M-d-Y") ?>" class="csv_export"><span class="icon-file-excel"></span> Export Table</a></li>
+                        <?php } ?>
+                    </ul>
                 </div>
-                <div class="x_content">
+            </div>
+        </div>
+    </div>
+    <!-- /page header -->
+
+    <!-- Content area -->
+    <div class="content">
+
+    <!-- page content -->
+    <div class="right_col" role="main">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="panel panel-flat">
+                <div class="panel-body">
                     <div id='containerList'><center style="min-height: 100vh;"><img src='<?php echo assets_dir("images/animal.gif"); ?>' ></center></div>
                 </div><!-- end of .x_content -->
             </div><!-- end of .x_panel -->
         </div><!-- end of div -->
-    </div><!-- end of .clearflash -->
-</div><!-- end of .right_col -->
+    </div><!-- end of .right_col -->
 
 <script type="text/javascript">
     $(document).ready(function () {
